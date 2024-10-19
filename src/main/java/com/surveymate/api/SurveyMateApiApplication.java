d154e14883e2abc70e486853367ae42555b7ff51
dajeong <uwhami@gmail.com>
2024-10-19 21:48:19 +0900

    initial commit

---
+++ src/main/java/com/surveymate/api/SurveyMateApiApplication.java
@@ -0,0 +1,13 @@
+package com.surveymate.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SurveyMateApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SurveyMateApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/surveymate/api/SurveyMateApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.surveymate.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SurveyMateApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
