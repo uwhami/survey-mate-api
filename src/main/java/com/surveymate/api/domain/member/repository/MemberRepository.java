5c625e973c363a213350c1010622ebff2a0e4775
dajeong <uwhami@gmail.com>
2024-10-23 23:02:54 +0900

    security 설정 + member create

---
+++ src/main/java/com/surveymate/api/domain/member/repository/MemberRepository.java
@@ -0,0 +1,14 @@
+package com.surveymate.api.domain.member.repository;
+
+import com.surveymate.api.domain.member.entity.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+public interface MemberRepository extends JpaRepository<Member, String> {
+
+
+    @Query("SELECT m.memNum FROM Member m WHERE m.memNum LIKE CONCAT('M', :today, '%') ORDER BY m.memNum DESC")
+    String findTopMemNumByToday(@Param("today") String today);
+
+}
---
+++ src/test/java/com/surveymate/api/member/MemberRepositoryTests.java
@@ -0,0 +1,37 @@
+package com.surveymate.api.member;
+
+import com.surveymate.api.domain.member.entity.Member;
+import com.surveymate.api.domain.member.mapper.MemberMapper;
+import com.surveymate.api.domain.member.repository.MemberRepository;
+import lombok.extern.log4j.Log4j2;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@Log4j2
+@SpringBootTest
+public class MemberRepositoryTests {
+
+    @Autowired
+    MemberRepository memberRepository;
+
+
+    @Test
+    public void save() {
+        Member member = Member.builder()
+                .memNum("")
+                .userId("user123")
+                .password("encodedPassword")
+                .userName("John Doe")
+                .profileImage("profile.jpg")
+                .joinDate("20231022")
+                .memStatus("A")
+                .build();
+        memberRepository.save(member);
+
+        for(Member m : memberRepository.findAll()) {
+            System.out.println("==========Member-----=========");
+            System.out.println(m.toString());
+        }
+    }
+}
