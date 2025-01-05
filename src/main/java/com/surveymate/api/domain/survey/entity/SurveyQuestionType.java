19421d7b2cd09fb6db31beb0d9d2213b72de2aea
dajeong <uwhami@gmail.com>
2025-01-05 12:59:39 +0900

    설문 유형 테이블, 데이터 수정

--- src/main/java/com/surveymate/api/domain/survey/entity/SurveyQuestionType.java
+++ src/main/java/com/surveymate/api/domain/survey/entity/SurveyQuestionType.java
@@ -1,6 +1,9 @@
 package com.surveymate.api.domain.survey.entity;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -11,8 +14,8 @@ import lombok.NoArgsConstructor;
 public class SurveyQuestionType {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    @Column(nullable = false, unique = true)
+    private String id;
 
     @Column(nullable = false, unique = true)
     private String name; // 질문 유형 이름 (예: RADIO, CHECKBOX, TEXT)
@@ -20,7 +23,8 @@ public class SurveyQuestionType {
     @Column(nullable = false)
     private String description; // 질문 유형 설명 (예: "단일 선택형 질문")
 
-    public SurveyQuestionType(String name, String description) {
+    public SurveyQuestionType(String id, String name, String description) {
+        this.id = id;
         this.name = name;
         this.description = description;
     }
