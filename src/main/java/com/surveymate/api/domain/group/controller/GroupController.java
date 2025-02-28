ce56e0c627e2e494faca22139e6af0deca4fcbf7
dajeong <uwhami@gmail.com>
2025-02-28 23:51:35 +0900

    restful 방식으로 변경.

--- src/main/java/com/surveymate/api/domain/group/controller/GroupController.java
+++ src/main/java/com/surveymate/api/domain/group/controller/GroupController.java
@@ -1,10 +1,10 @@
 package com.surveymate.api.domain.group.controller;
 
 import com.surveymate.api.domain.group.dto.GroupReponse;
-import com.surveymate.api.domain.group.dto.GroupRequest;
 import com.surveymate.api.domain.group.service.GroupService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,9 +15,9 @@ public class GroupController {
 
     private final GroupService groupService;
 
-    @GetMapping("/getGroupInfo")
-    public GroupReponse getGroupInfo(GroupRequest groupRequest) {
-        return groupService.selectGroupByUserId(groupRequest);
+    @GetMapping("/{groupId}")
+    public GroupReponse getGroupInfo(@PathVariable Long groupId) {
+        return groupService.selectGroupByUserId(groupId);
     }
 
 }
