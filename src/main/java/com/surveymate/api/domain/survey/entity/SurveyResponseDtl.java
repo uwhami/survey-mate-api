a4871405c539579cba1edeef21669a894ef33054
jieun <jein6316@gmail.com>
2025-03-15 15:28:16 +0900

    엔티티 추가 수정

--- src/main/java/com/surveymate/api/domain/survey/entity/SurveyResponseDtl.java
+++ src/main/java/com/surveymate/api/domain/survey/entity/SurveyResponseDtl.java
@@ -16,19 +16,19 @@ public class SurveyResponseDtl extends BaseEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id; // 응답 상세 ID (PK)
 
-    @ManyToOne(fetch = FetchType.LAZY)
+    /*@ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "response_id", nullable = false)
-    private SurveyResponseMst response; // 응답 마스터와의 관계
+    private SurveyResponseMst response; // 응답 마스터와의 관계*/
 
-    @ManyToOne(fetch = FetchType.LAZY)
+    /*@ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "questionDtl_id", nullable = false)
-    private SurveyQuestionDtl question; // 질문 디테일과의 관계
+    private SurveyQuestionDtl question; // 질문 디테일과의 관계*/
 
     @Column(nullable = true)
     private String answerText; // 주관식 답변 (null 가능)
 
-    @ManyToOne(fetch = FetchType.LAZY)
+    /*@ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "option_id", nullable = true)
-    private SurveyQuestionSdtl option; // 선택된 옵션 (객관식의 경우)
+    private SurveyQuestionSdtl option; // 선택된 옵션 (객관식의 경우)*/
 
 }
