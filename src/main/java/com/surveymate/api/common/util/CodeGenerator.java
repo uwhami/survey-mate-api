a00e9e8b67af57aae10d89dfa9477d6e90cb53d7
dajeong <uwhami@gmail.com>
2025-03-15 15:31:49 +0900

    SurveyQuestionMst 공통 코드 생성 개발.

--- src/main/java/com/surveymate/api/common/util/CodeGenerator.java
+++ src/main/java/com/surveymate/api/common/util/CodeGenerator.java
@@ -1,6 +1,7 @@
 package com.surveymate.api.common.util;
 
 import com.surveymate.api.domain.member.repository.MemberRepository;
+import com.surveymate.api.domain.survey.repository.SurveyQuestionMstRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Component;
@@ -16,6 +17,7 @@ public class CodeGenerator {
     private static final SimpleDateFormat DATE_FORMAT = new SimpleDateFormat("yyyyMMdd");
 
     private final MemberRepository memberRepository;
+    private final SurveyQuestionMstRepository surveyQuestionMstRepository;
 
     public String generateCode(String code) {
         String today = DATE_FORMAT.format(new Date());
@@ -25,7 +27,10 @@ public class CodeGenerator {
             if ("MU01".equals(code)) {
                 prefixCode = "M";
                 topNum = memberRepository.findTopMemNumByToday(today);
-            } else {
+            }else if("SQ01".equals(code)){
+                prefixCode = "SQ";
+                topNum = surveyQuestionMstRepository.findTopSqNumByToday(today);
+            }else {
                 throw new IllegalArgumentException("잘못된 코드 번호입니다.");
             }
         } catch (Exception e) {
