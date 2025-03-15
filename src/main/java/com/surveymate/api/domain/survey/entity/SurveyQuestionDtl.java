d7b0a695737a22220a8873df46eb3a21b2b6363d
jieun <jein6316@gmail.com>
2025-03-15 16:02:41 +0900

    type join 관계 제거

--- src/main/java/com/surveymate/api/domain/survey/entity/SurveyQuestionDtl.java
+++ src/main/java/com/surveymate/api/domain/survey/entity/SurveyQuestionDtl.java
@@ -2,6 +2,7 @@ package com.surveymate.api.domain.survey.entity;
 
 import com.surveymate.api.common.entity.BaseEntity;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -9,21 +10,15 @@ import lombok.NoArgsConstructor;
 @Table(name = "survey_question_dtl")
 @Getter
 @NoArgsConstructor
+@AllArgsConstructor
 public class SurveyQuestionDtl extends BaseEntity {
 
     @EmbeddedId
     private SurveyQuestionDtlId id; // 복합 키
 
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "typeId", nullable = false)
-    private SurveyQuestionType type; // 질문 유형 ID (FK to SurveyQuestionType)
+    @Column(nullable = false)
+    private String typeId; // 질문 유형 ID (FK to SurveyQuestionType)
 
     @Column(nullable = false)
     private String questionText; // 질문 내용
-
-    public SurveyQuestionDtl(SurveyQuestionDtlId id, SurveyQuestionType type, String questionText) {
-        this.id = id;
-        this.type = type;
-        this.questionText = questionText;
-    }
 }
