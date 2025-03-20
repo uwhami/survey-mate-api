9c6cbba4c886dc6984c0065832c788a52b6ea1b7
dajeong <uwhami@gmail.com>
2025-03-20 23:36:51 +0900

    설문조사 질문지 작성 내용 불러오기 개발.

--- src/main/java/com/surveymate/api/domain/survey/controller/SurveyResponseController.java
+++ src/main/java/com/surveymate/api/domain/survey/controller/SurveyResponseController.java
@@ -1,20 +1,25 @@
 package com.surveymate.api.domain.survey.controller;
 
-import com.surveymate.api.domain.survey.dto.SurveyQuestionResponse;
+import com.surveymate.api.domain.survey.dto.SurveyResponseForm;
+import com.surveymate.api.domain.survey.service.SurveyResponseService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/api/survey/response")
 public class SurveyResponseController {
 
-    @GetMapping("/{surveyurl}")
-    public SurveyQuestionResponse getSurveyForm(@PathVariable String url) {
-        return null;
+    SurveyResponseService surveyResponseService;
+
+    @GetMapping("/{surveyUrl}")
+    public List<SurveyResponseForm> getSurveyForm(@PathVariable String surveyUrl) {
+        return surveyResponseService.getSurveyForm(surveyUrl);
     }
 
 }
--- src/test/java/com/surveymate/api/survey/SurveyServiceTests.java
+++ src/test/java/com/surveymate/api/survey/SurveyServiceTests.java
@@ -34,13 +34,30 @@ public class SurveyServiceTests {
         surveyQuestionMstRepository.save(mst);
 
         SurveyQuestionDtlId dtlId = new SurveyQuestionDtlId(mst, 1);
-//        SurveyQuestionType type1 = new SurveyQuestionType("SQT001", "RADIO", "단일 선택형 질문");
+        SurveyQuestionDtlId dtlId2 = new SurveyQuestionDtlId(mst, 2);
         SurveyQuestionDtl dtl = new SurveyQuestionDtl(dtlId, "SQT001", "질문1");
+        SurveyQuestionDtl dtl2 = new SurveyQuestionDtl(dtlId2, "SQT001", "질문2");
         surveyQuestionDtlRepository.save(dtl);
+        surveyQuestionDtlRepository.save(dtl2);
 
         SurveyQuestionSdtlId sdtlId = new SurveyQuestionSdtlId(dtl, 1);
-        SurveyQuestionSdtl sdtl = new SurveyQuestionSdtl(sdtlId, "optionTest");
+        SurveyQuestionSdtlId sdtlId2 = new SurveyQuestionSdtlId(dtl, 2);
+        SurveyQuestionSdtl sdtl = new SurveyQuestionSdtl(sdtlId, "radio1");
+        SurveyQuestionSdtl sdtl2 = new SurveyQuestionSdtl(sdtlId2, "radio2");
         surveyQuestionSdtlRepository.save(sdtl);
+        surveyQuestionSdtlRepository.save(sdtl2);
+
+        SurveyQuestionSdtlId sdtlId2_1 = new SurveyQuestionSdtlId(dtl2, 1);
+        SurveyQuestionSdtlId sdtlId2_2 = new SurveyQuestionSdtlId(dtl2, 2);
+        SurveyQuestionSdtlId sdtlId2_3 = new SurveyQuestionSdtlId(dtl2, 2);
+
+        SurveyQuestionSdtl sdtl2_1 = new SurveyQuestionSdtl(sdtlId2_1, "check1");
+        SurveyQuestionSdtl sdtl2_2 = new SurveyQuestionSdtl(sdtlId2_2, "check2");
+        SurveyQuestionSdtl sdtl2_3 = new SurveyQuestionSdtl(sdtlId2_3, "check3");
+        surveyQuestionSdtlRepository.save(sdtl2_1);
+        surveyQuestionSdtlRepository.save(sdtl2_2);
+        surveyQuestionSdtlRepository.save(sdtl2_3);
+
 
     }
 
