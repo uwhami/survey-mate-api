3cda4ee6bbbc02d5546e2d2059de7699b738e43a
jieun <jein6316@gmail.com>
2025-03-22 13:48:56 +0900

    dto 푸쉬

--- src/main/java/com/surveymate/api/domain/survey/dto/SurveyQuestionSdtlResponse.java
+++ src/main/java/com/surveymate/api/domain/survey/dto/SurveyQuestionSdtlResponse.java
@@ -5,18 +5,13 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import jakarta.validation.constraints.NotNull;
-
-
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
-public class SurveyQuestionSdtlRequest {
+public class SurveyQuestionSdtlResponse {
 
-    @NotNull(message = "Option text cannot be null")
+    private Integer questionSdtlOrder; // 선택지 순서
     private String optionText; // 선택지 내용
 
-    @NotNull(message = "Option order cannot be null")
-    private Integer questionSdtlOrder; // 선택지 순서
 }
