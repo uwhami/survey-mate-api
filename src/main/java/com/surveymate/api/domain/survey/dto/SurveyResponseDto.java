0cc3614bb9fefed8a9fd6e204d0ca7841ddc1af8
dajeong <uwhami@gmail.com>
2025-03-29 13:09:58 +0900

    설문조사 응답 제출 개발

--- src/main/java/com/surveymate/api/domain/survey/dto/SurveyResponseDto.java
+++ src/main/java/com/surveymate/api/domain/survey/dto/SurveyResponseDto.java
@@ -1,21 +1,16 @@
 package com.surveymate.api.domain.survey.dto;
 
 
+import com.surveymate.api.common.dto.MemnumAware;
 import lombok.Getter;
 
 import java.util.List;
 
 
 @Getter
-public class SurveyResponseDto {
+public class SurveyResponseDto extends MemnumAware {
 
     private String sqMstId;
-    private List<SurveyResponse> surveyResponse;
+    private List<SurveyResponseDtlDto> surveyResponse;
 
-
-    @Getter
-    static class SurveyResponse{
-        private int questionId;
-        private List<String> answer;
-    }
 }
--- src/main/java/com/surveymate/api/domain/survey/service/SurveyResponseService.java
+++ src/main/java/com/surveymate/api/domain/survey/service/SurveyResponseService.java
@@ -1,9 +1,12 @@
 package com.surveymate.api.domain.survey.service;
 
 import com.surveymate.api.domain.survey.dto.SurveyQuestionMstResponse;
+import com.surveymate.api.domain.survey.dto.SurveyResponseDto;
 
 
 public interface SurveyResponseService {
 
     SurveyQuestionMstResponse getSurveyForm(String surveyUrl);
+
+    void saveResponseData(SurveyResponseDto responseDto);
 }
