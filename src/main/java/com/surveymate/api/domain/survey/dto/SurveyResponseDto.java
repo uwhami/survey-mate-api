9a3115e9d58ecb8997abbafc89659fcfb7aacaca
dajeong <uwhami@gmail.com>
2025-04-09 21:34:25 +0900

    Add survey response list feature

--- src/main/java/com/surveymate/api/domain/survey/dto/SurveyResponseDto.java
+++ src/main/java/com/surveymate/api/domain/survey/dto/SurveyResponseDto.java
@@ -2,16 +2,20 @@ package com.surveymate.api.domain.survey.dto;
 
 
 import com.surveymate.api.common.dto.MemInfoAware;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import java.util.List;
 
+@AllArgsConstructor
 @NoArgsConstructor
 @Getter
 public class SurveyResponseDto extends MemInfoAware {
 
     private String sqMstId;
+    private String title;
+    private String description;
     private String surveyUrl;
     private List<SurveyResponseDtlDto> surveyResponse;
 
